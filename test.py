2149004ab9c5d82f8b29f4af860a69f1d9fc1dfe
anshikamittal22199 <46889844+anshikamittal22199@users.noreply.github.com>
2021-10-25 21:36:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import main
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(main.add(10, 5, 6), 15)
+        self.assertEqual(main.add(10, 5,), 15)
         
 
     def test_subtract(self):
