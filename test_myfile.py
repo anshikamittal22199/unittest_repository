2218427293bc423488485f1fcfb014fa6cbdd3a8
anshikamittal22199 <46889844+anshikamittal22199@users.noreply.github.com>
2021-10-25 13:21:55 +0530

    Update test_myfile.py

--- test_myfile.py
+++ test_myfile.py
@@ -1,7 +1,13 @@
 import unittest
-class TestCuboid(unittest.TestCase):
-    def test_volume(self):
-        self.assertAlmostEqual(cuboid_volume(2),8)
-        self.assertAlmostEqual(cuboid_volume(1),1)
-        self.assertAlmostEqual(cuboid_volume(0),0)
-        self.assertAlmostEqual(cuboid_volume(5.5),166.375)
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
